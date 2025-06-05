ea7ece4c3ff158595384db31a833aec70e0bc153
Witor Oliveira <witoredson@gmail.com>
2025-06-05 16:24:23 -0300

    feat: was optimized the SimpleAgent class internal prompt to consume fewer tokens when possible and to me more objective

--- src/tyr_agent/core/agent.py
+++ src/tyr_agent/core/agent.py
@@ -33,19 +33,7 @@ class SimpleAgent(FileMixin):
         self.agent_model: genai.GenerativeModel = model
 
         self.MAX_HISTORY = min(max_history, self.MAX_ALLOWED_HISTORY)
-        self.PROMPT_TEMPLATE = """{role}
-
-Você pode usar o histórico de conversas abaixo para responder perguntas relacionadas a interações anteriores com o usuário. 
-Se o usuário perguntar sobre algo que já foi dito anteriormente, procure a informação no histórico.
-
-Cada resposta do agente no histórico pode conter uma nota de 0 a 5, representando o quanto ela foi útil para o usuário. 
-Use essas notas como um indicativo da qualidade da resposta anterior. Priorize informações com notas mais altas e busque manter esse nível de qualidade em sua resposta atual.
-
-Histórico de Conversas:
-{history}
-
-Com base na mensagem atual, gere uma resposta natural para o usuário.
-{current}"""
+        self.PROMPT_TEMPLATE = ""
 
     async def chat(self, user_input: str, streaming: bool = False, files: Optional[List[dict]] = None, save_history: bool = True) -> Optional[str]:
         try:
@@ -74,28 +62,42 @@ Com base na mensagem atual, gere uma resposta natural para o usuário.
     def __generate_prompt(self, prompt_text: str) -> str:
         try:
             if not self.use_history or not self.history:
-                return self.PROMPT_TEMPLATE.format(
-                    role=self.prompt_build,
-                    history='Não consta.',
-                    current=prompt_text
+                formatted_history = False
+            else:
+                def insert_score(score: Union[int, float, float]):
+                    if self.use_score:
+                        return f" - Score: {str(score) + '/5' if score is not None else 'Não consta'}"
+                    else:
+                        return ''
+
+                formatted_history = "\n".join(
+                    f"{item['timestamp']}{insert_score(item['score'])}\nUser: {item['interaction']['user']}\nAgent: {' | '.join(item['interaction']['agent'])}"
+                    for item in self.history
                 )
 
-            def insert_score(score: Union[int, float, float]):
-                if self.use_score:
-                    return f" - Score: {str(score) + '/5' if score is not None else 'Não consta'}"
-                else:
-                    return ''
+            first_prompt_template: str = f"{self.prompt_build}\n"
 
-            formatted_history = "\n".join(
-                f"{item['timestamp']}{insert_score(item['score'])}\nUser: {item['interaction']['user']}\nAgent: {' | '.join(item['interaction']['agent'])}"
-                for item in self.history
-            )
+            if self.use_history:
+                second_prompt_template: str = f"""
+Você pode usar o histórico de conversas abaixo para responder perguntas relacionadas a interações anteriores com o usuário. 
+Se o usuário perguntar sobre algo que já foi dito anteriormente, procure a informação no histórico.
+{
+'''\nCada resposta do agente no histórico pode conter uma nota de 0 a 5, representando o quanto ela foi útil para o usuário. 
+Use essas notas como um indicativo da qualidade da resposta anterior. Priorize informações com notas mais altas e busque manter esse nível de qualidade em sua resposta atual.\n''' if self.use_score else ''
+}
+Histórico de Conversas:
+{formatted_history if formatted_history else "Não Consta."}
+"""
+            else:
+                second_prompt_template: str = ""
 
-            return self.PROMPT_TEMPLATE.format(
-                role=self.prompt_build,
-                history=formatted_history if self.history else 'Não consta.',
-                current=prompt_text
-            )
+            third_prompt_template: str = f"""
+Gere uma resposta natural para o usuário com base na mensagem atual:
+{prompt_text}"""
+
+            final_prompt_template: str = first_prompt_template + second_prompt_template + third_prompt_template
+
+            return final_prompt_template
         except Exception as e:
             print(f'[ERROR] - Ocorreu um erro durante a geração do prompt: {e}')
             return ""
